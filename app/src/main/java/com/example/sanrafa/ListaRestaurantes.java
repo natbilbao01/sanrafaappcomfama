da501d9cd155cf91727d200786625ce4064e3fa4
julian <nmartinezbi@cesde.net>
2024-03-13 07:17:10 -0500

    trabajo final

--- app/src/main/java/com/example/sanrafa/ListaRestaurantes.java
+++ app/src/main/java/com/example/sanrafa/ListaRestaurantes.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.sanrafa.adaptadores.RestauranteAdaptador;
 import com.example.sanrafa.moldes.Restaurantes;
 
 import java.util.ArrayList;
@@ -20,18 +21,22 @@ public class ListaRestaurantes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_restaurantes);
 
+        crearlistarestaurantes();
+
         recyclerViewRestaurantes=findViewById(R.id.Listadinamicaderestaurantes);
         recyclerViewRestaurantes.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
 
+        RestauranteAdaptador restauranteAdaptador = new RestauranteAdaptador(ListaRestaurantes);
+        recyclerViewRestaurantes.setAdapter(restauranteAdaptador);
 
 
     }
     public void crearlistarestaurantes(){
-        ListaRestaurantes.add(new Restaurantes("La Aldea","20USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante02));
+        ListaRestaurantes.add(new Restaurantes("La Aldea","20USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante01));
         ListaRestaurantes.add(new Restaurantes("La Paila","10USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante02));
-        ListaRestaurantes.add(new Restaurantes("El pilon","15USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante02));
-        ListaRestaurantes.add(new Restaurantes("Maizal","30USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante02));
-        ListaRestaurantes.add(new Restaurantes("El Molino","35USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante02));
+        ListaRestaurantes.add(new Restaurantes("El pilon","15USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante05));
+        ListaRestaurantes.add(new Restaurantes("Maizal","30USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante03));
+        ListaRestaurantes.add(new Restaurantes("El Molino","35USD","Comida tipica de la region","6042589569","Calle principal",5,R.drawable.restaurante06));
 
     }
 
--- app/src/main/java/com/example/sanrafa/ListaSitios.java
+++ app/src/main/java/com/example/sanrafa/ListaSitios.java
@@ -1,14 +1,42 @@
 package com.example.sanrafa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.sanrafa.adaptadores.RestauranteAdaptador;
+import com.example.sanrafa.adaptadores.SitiosAdaptador;
+import com.example.sanrafa.moldes.Restaurantes;
+import com.example.sanrafa.moldes.Sitios;
+
+import java.util.ArrayList;
+
 public class ListaSitios extends AppCompatActivity {
+
+    ArrayList<com.example.sanrafa.moldes.Sitios>ListaSitios=new ArrayList<>();
+    RecyclerView recyclerViewListaSitios;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_sitios);
+
+        crearlistaSitios();
+        recyclerViewListaSitios=findViewById(R.id.Listadinamicadesitios);
+        recyclerViewListaSitios.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+
+        SitiosAdaptador SitiosAdaptador = new SitiosAdaptador(ListaSitios);
+        recyclerViewListaSitios.setAdapter(SitiosAdaptador);
+    }
+
+    private void crearlistaSitios() {
+        ListaSitios.add(new Sitios("Monumento al Buey","Lugar emblematico","85069874","Al lado de la iglesia","todo el dia","gratis","ropa comoda",4,R.drawable.buey));
+        ListaSitios.add(new Sitios("Tronco de la ceiba","Lugar emblematico","85062053","Parque principal","todo el dia","gratis","ropa comoda",5,R.drawable.ceiba));
+        ListaSitios.add(new Sitios("Alto de la Virgen","Lugar de oracion","85052553","kilometro 3 ","todo el dia","gratis","ropa comoda",4,R.drawable.virgen));
+        ListaSitios.add(new Sitios("Charco Quevedo","Lugar para el descanso","85062053","via tamesis","todo el dia","gratis","ropa comoda",5,R.drawable.charco));
+        ListaSitios.add(new Sitios("Rapel","Deporte Extremo","85062512","Via pintada","De 10am a 3pm","$40USD","ropa comoda",5,R.drawable.rapel001));
     }
 }
